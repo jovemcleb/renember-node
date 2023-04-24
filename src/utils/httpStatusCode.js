0430f7ffdf9b0d92bea949c668166b488b232d83
jovemcleb <lima.cardoso.caleb@gmail.com>
2023-04-24 18:08:44 -0300

    feat: adiciona util httpStatus code

---
+++ src/utils/httpStatusCode.js
@@ -0,0 +1,13 @@
+const httpStatusCode = {
+  OK: 200,
+  CREATED: 201,
+  NO_CONTENT: 204,
+  BAD_REQUEST: 400,
+  UNAUTHORIZED: 401,
+  NOT_FOUND: 404,
+  CONFLICT: 409,
+  INTERNAL_SERVER: 500,
+  UNPROCESSABLE_ENTITY: 422,
+};
+
+module.exports = { httpStatusCode };
