098830d311f32291866b1363a8deb034567da8a6
jovemcleb <lima.cardoso.caleb@gmail.com>
2023-04-24 21:35:46 -0300

    feat: remove function inútil

--- server.js
+++ server.js
@@ -6,10 +6,6 @@ connectToDataBase();
 
 const port = process.env.PORT;
 
-app.get("/", (_req, res) => {
-  res.send();
-});
-
 app.listen(port, () => {
   console.log(`Rodando na porta: ${port}`);
 });
